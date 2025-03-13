6e3041943543e0ce8801b1430ddd6d5db9664b96
DucTai <ductai.dt05@gmail.com>
2025-03-13 14:45:52 +0700

    LAB_W1-from-HCMUS-C202

--- LAB_W1/23122013.cpp
+++ LAB_W1/23122013.cpp
@@ -15,6 +15,20 @@ public:
     void OutputDate();
     bool CheckDate();
     bool InspectLeapYear();
+
+    CDate IncreaseYear();
+    CDate IncreaseMonth();
+    CDate IncreaseDay();
+    CDate DecreaseYear();
+    CDate DecreaseMonth();
+    CDate DecreaseDay();
+
+    CDate IncreaseYear(int);
+    CDate IncreaseMonth(int);
+    CDate IncreaseDay(int);
+    CDate DecreaseYear(int);
+    CDate DecreaseMonth(int);
+    CDate DecreaseDay(int);
 };
 
 void CDate::InputDate()
@@ -63,13 +77,163 @@ bool CDate::CheckDate()
 
 bool CDate::InspectLeapYear()
 {
+    if (Year % 400 == 0)
+        return true; // mod400 == 0
+    if (Year % 100 == 0)
+        return false; // mod100 == 0 && mod400 != 0
+    if (Year % 4 == 0)
+        return true;
     return false;
+}
+
+CDate CDate::IncreaseYear()
+{
+    Year++;
+    while (CheckDate() == false)
+        Day--;
+    return *this;
+}
+
+CDate CDate::IncreaseMonth()
+{
+    Month++;
+    if (Month > 12)
+    {
+        Year++;
+        Month = 1;
+    }
+    while (CheckDate() == false)
+        Day--;
+    return *this;
+}
+
+CDate CDate::IncreaseDay()
+{
+    Day++;
+    if (CheckDate() == false)
+    {
+        Month++;
+        Day = 1;
+    }
+    if (CheckDate() == false)
+    {
+        Month = 1;
+        Year++;
+    }
+    return *this;
+}
+
+CDate CDate::DecreaseYear()
+{
+    Year--;
+    while (CheckDate() == false)
+        Day--;
+    return *this;
+}
+
+CDate CDate::DecreaseMonth()
+{
+    Month--;
+    if (Month == 0)
+    {
+        Year--;
+        Month = 12;
+    }
+    while (CheckDate() == false)
+        Day--;
+    return *this;
+}
+
+CDate CDate::DecreaseDay()
+{
+    Day--;
+    if (Day == 0)
+    {
+        Month -= 1;
+        if (Month == 0)
+        {
+            Year--;
+            Month = 12;
+        }
+    }
+    if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
+        Day = 31;
+    else if (Month == 2)
+    {
+        if (InspectLeapYear())
+            Day = 29;
+        else
+            Day = 28;
+    }
+    else
+        Day = 30;
+    return *this;
+}
+
+CDate CDate::IncreaseYear(int a)
+{
+    CDate newDate = *this;
+    while (a--)
+        newDate = newDate.IncreaseYear();
+    *this = newDate;
+    return newDate;
+}
+
+CDate CDate::IncreaseMonth(int a)
+{
+    CDate newDate = *this;
+    while (a--)
+        newDate = newDate.IncreaseMonth();
+    *this = newDate;
+    return newDate;
+}
+
+CDate CDate::IncreaseDay(int a)
+{
+    CDate newDate = *this;
+    while (a--)
+        newDate = newDate.IncreaseDay();
+    *this = newDate;
+    return newDate;
+}
+
+CDate CDate::DecreaseYear(int a)
+{
+    CDate newDate = *this;
+    while (a--)
+        newDate = newDate.DecreaseYear();
+    *this = newDate;
+    return newDate;
+}
+
+CDate CDate::DecreaseMonth(int a)
+{
+    CDate newDate = *this;
+    while (a--)
+        newDate = newDate.DecreaseMonth();
+    *this = newDate;
+    return newDate;
+}
+
+CDate CDate::DecreaseDay(int a)
+{
+    CDate newDate = *this;
+    while (a--)
+        newDate = newDate.DecreaseDay();
+    *this = newDate;
+    return newDate;
 }
 
 int main()
 {
     CDate day1;
     day1.InputDate();
+    cout << (day1.CheckDate() ? "True\n" : "False\n");
+    day1.IncreaseYear(5);
     day1.OutputDate();
-    cout << (day1.CheckDate() ? "True" : "False");
+    day1.IncreaseMonth(3);
+    day1.OutputDate();
+    day1.IncreaseDay(2);
+    day1.OutputDate();
+    return 0;
 }
---
+++ LAB_W1/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+day1.InputDate();
+    cout << (day1.CheckDate() ? "True\n" : "False\n");
+    day1.IncreaseYear();
+    day1.OutputDate();
+    day1.IncreaseMonth();
+    day1.OutputDate();
+    day1.IncreaseDay();
+    day1.IncreaseDay();
+    day1.OutputDate();
